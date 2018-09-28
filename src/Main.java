f4b96d4ad477be7745a0894e4df0dfbd6cc5ccd0
zapp91 <zapp917@yahoo.com>
2018-09-27 23:09:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//Written By: Royce Duncan, 9/27/2018
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PreferredCustomer cusNum1 = new PreferredCustomer("Royce", "Duncan", "123 myAddress Rd", "8508909756", 1, false, 430.50f, 1);
+		PreferredCustomer cusNum2 = new PreferredCustomer("Royce", "Duncan", "123 myAddress Rd", "8508909756", 1, false, 430.50f, 1);
+		PreferredCustomer cusNum3 = new PreferredCustomer("Royce", "Duncan", "123 myAddress Rd", "8508909756", 1, false, 430.50f, 1);
+		PreferredCustomer cusNum4 = new PreferredCustomer("Royce", "Duncan", "123 myAddress Rd", "8508909756", 1, false, 430.50f, 1);
+				
+		
+		
+		
+	}
+
+}
