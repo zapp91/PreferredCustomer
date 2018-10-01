b938052364812bf79a80a35c8adc170507719507
zapp91 <zapp917@yahoo.com>
2018-09-30 22:28:59 -0500

    save

--- src/Window.java
+++ src/Window.java
@@ -37,8 +37,7 @@ public class Window extends JFrame {
 	
 	private void buildCheckout() {
 		
-		//create main panels
-		JPanel topPanel = new JPanel();
+		//create main panel
 		JPanel mainPanel = new JPanel();
 		
 		//create sub panels
@@ -84,8 +83,6 @@ public class Window extends JFrame {
 		pendingPaymentField.setEditable(false);
 		
 		//add main panel features
-		panel1.add(addNewCustomerButton);
-		
 		panel2.add(customerLabel);
 		panel2.add(nameBox);
 		
@@ -109,7 +106,6 @@ public class Window extends JFrame {
 		mainPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		mainPanel.setPreferredSize(new Dimension(260, 220));
 		
-		topPanel.add(panel1);
 		mainPanel.add(panel2);
 		mainPanel.add(panel3);
 		mainPanel.add(panel4);
@@ -117,88 +113,9 @@ public class Window extends JFrame {
 		mainPanel.add(panel6);
 		mainPanel.add(panel7);
 		
-		add(topPanel);
 		add(mainPanel);
 		add(panel8);
 
-	}
-	
-	private void buildAddCus() {
-		
-		//removeAll(); //or remove(JComponent)
-		//revalidate();
-		//repaint();
-		
-		//create main panel
-		JPanel mainPanel = new JPanel();
-		
-		//create sub panels
-		JPanel panel1 = new JPanel();
-		JPanel panel2 = new JPanel();
-		JPanel panel3 = new JPanel();
-		JPanel panel4 = new JPanel();
-		JPanel panel5 = new JPanel();
-		JPanel panel6 = new JPanel();
-		JPanel panel7 = new JPanel();
-		JPanel panel8 = new JPanel();
-		
-		//create labels
-		JLabel firstNameLabel = new JLabel("First Name ");
-		JLabel lastNameLabel = 	new JLabel("Last Name ");
-		JLabel addressLabel = 	new JLabel("Address ");
-		JLabel phoneLabel = 	new JLabel("Phone ");
-		JLabel spentLabel =		new JLabel("Money Spent ");
-		JLabel mailListLabel = 	new JLabel("Mail List ");
-		
-		//create text fields
-		JTextField firstNameField = new JTextField(10);
-		JTextField lastNameField = 	new JTextField(10);
-		JTextField addressField = 	new JTextField(10);
-		JTextField phoneField =		new JTextField(10);
-		JTextField spentField = 	new JTextField(10);
-		
-		//create three buttons
-		JButton clearButton = new JButton("Clear");
-		JButton addCusButton = new JButton("Add Customer");
-		JButton returnButton = new JButton("Return to Checkout");
-		//addNewCustomerButton.addActionListener(new addCusButtonListener());
-		
-		//add main panel features
-		panel1.add(firstNameLabel);
-		panel1.add(firstNameField);
-		
-		panel2.add(lastNameLabel);
-		panel2.add(lastNameField);
-		
-		panel3.add(addressLabel);
-		panel3.add(addressField);
-		
-		panel4.add(phoneLabel);
-		panel4.add(phoneField);
-		
-		panel5.add(spentLabel);
-		panel5.add(spentField);
-		
-		//panel6.add();
-		//panel6.add();
-		
-		panel7.add(clearButton);
-		panel7.add(addCusButton);
-		
-		panel8.add(returnButton);
-		
-		mainPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		mainPanel.setPreferredSize(new Dimension(260, 220));
-		
-		mainPanel.add(panel1);
-		mainPanel.add(panel2);
-		mainPanel.add(panel3);
-		mainPanel.add(panel4);
-		mainPanel.add(panel5);
-		mainPanel.add(panel6);
-		mainPanel.add(panel7);
-		
-		add(mainPanel);
 	}
 	
 	private class addCusButtonListener implements ActionListener {
@@ -207,7 +124,7 @@ public class Window extends JFrame {
 			
 			System.out.println("running buildAddCus()");
 			
-			buildAddCus();
+			
 			
 		}
 		
