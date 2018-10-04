34907dd17957833a66b227dcb2f133c0674dcf49
Royce <zapp917@yahoo.com>
2018-10-04 10:30:05 -0500

    built add customer window

--- src/Window.java
+++ src/Window.java
@@ -23,6 +23,10 @@ public class Window extends JFrame {
 	JButton addNewCustomerButton;
 	JButton payButton;
 	
+	JButton clearButton;
+	JButton addCusButton;
+	JButton returnButton;
+	
 	JComboBox nameBox;
 	
 	JLabel customerLabel;		
@@ -32,12 +36,29 @@ public class Window extends JFrame {
 	JLabel amountDiscountedLabel;
 	JLabel pendingPaymentLabel;
 	
+	JLabel firstNameLabel;
+	JLabel lastNameLabel;
+	JLabel addressLabel;
+	JLabel phoneLabel;
+	JLabel moneySpentLabel; 
+	JLabel mailListLabel;
+	
 	JTextField currentSpentField;
 	JTextField currentDiscountField;
 	JTextField transactionAmountField;
 	JTextField amountDiscountedField;
 	JTextField pendingPaymentField;
 	
+	JTextField firstNameField;
+	JTextField lastNameField; 
+	JTextField addressField;	
+	JTextField phoneField;
+	JTextField moneySpentField;
+	
+	ButtonGroup mailListRadioGroup;
+	JRadioButton radio1;
+	JRadioButton radio2;
+
 	private final int WINDOW_WIDTH = 300;
 	private final int WINDOW_HEIGHT = 370;
 	
@@ -174,11 +195,113 @@ public class Window extends JFrame {
 
 	}
 	
+	private void buildAddCus() {
+		getContentPane().removeAll();//or remove(JComponent)
+		
+		//create main panel
+		mainPanel = new JPanel();
+		
+		//create sub panels
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		panel3 = new JPanel();
+		panel4 = new JPanel();
+		panel5 = new JPanel();
+		panel6 = new JPanel();
+		panel7 = new JPanel();
+		panel8 = new JPanel();
+		
+		//buttons
+		clearButton = new JButton("Clear");
+		addCusButton = new JButton("Add Customer");
+		returnButton = new JButton("Return to Check Out");
+		
+		//radio buttons
+		radio1 = new JRadioButton("Yes", true);
+		radio2 = new JRadioButton("No");
+		
+		mailListRadioGroup = new ButtonGroup();
+		mailListRadioGroup.add(radio1);
+		mailListRadioGroup.add(radio2);
+		
+		//create labels
+		firstNameLabel = 	new JLabel("First Name ");
+		lastNameLabel = 	new JLabel("Last Name ");
+		addressLabel = 		new JLabel("Address ");
+		phoneLabel =		new JLabel("Phone ");
+		moneySpentLabel =	new JLabel("Money Spent ");
+		mailListLabel =		new JLabel("Mail List ");
+		
+		//create text fields
+		firstNameField = 	new JTextField(10);
+		lastNameField = 	new JTextField(10);
+		addressField =		new JTextField(10);
+		phoneField =		new JTextField(10);
+		moneySpentField = 	new JTextField(10);
+		
+		//change component sizes
+		nameBox.setPreferredSize(new Dimension(115, 25));
+		
+		//change some text fields to uneditable
+		currentSpentField.setEditable(false);
+		currentDiscountField.setEditable(false);
+		amountDiscountedField.setEditable(false);
+		pendingPaymentField.setEditable(false);
+		
+		//listeners
+		addNewCustomerButton.addActionListener(new addCusButtonListener());
+		nameBox.addActionListener(new nameComboBoxListener());
+		transactionAmountField.addActionListener(new transactionAmountFieldListener());
+		
+		
+		//add main panel features
+		panel1.add(firstNameLabel);
+		panel1.add(firstNameField);
+		
+		panel2.add(lastNameLabel);
+		panel2.add(lastNameField);
+		
+		panel3.add(addressLabel);
+		panel3.add(addressField);
+		
+		panel4.add(phoneLabel);
+		panel4.add(phoneField);
+		
+		panel5.add(moneySpentLabel);
+		panel5.add(moneySpentField);
+		
+		panel6.add(mailListLabel);
+		panel6.add(radio1);
+		panel6.add(radio2);
+		
+		panel7.add(clearButton);
+		panel7.add(addCusButton);
+
+		panel8.add(returnButton);
+		
+		mainPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		mainPanel.setPreferredSize(new Dimension(260, 220));
+		
+		mainPanel.add(panel1);
+		mainPanel.add(panel2);
+		mainPanel.add(panel3);
+		mainPanel.add(panel4);
+		mainPanel.add(panel5);
+		mainPanel.add(panel6);
+			
+		add(mainPanel);
+		add(panel7);
+		add(panel8);
+		
+		revalidate();
+	}
+	
 	private class addCusButtonListener implements ActionListener {
 		
 		public void actionPerformed(ActionEvent e) {
 			
 			System.out.println("running buildAddCus()");
+			buildAddCus();
 		}
 	}
 	
@@ -205,6 +328,9 @@ public class Window extends JFrame {
 				
 				amountDiscountedField.setText("$" + String.format("%.2f", calculatedAmountDiscounted));
 				pendingPaymentField.setText("$" + String.format("%.2f", calculatedPendingPayment));
+				
+				//amountDiscountedField.setText("$" + calculatedAmountDiscounted);
+				//pendingPaymentField.setText("$" + calculatedPendingPayment);
 			}
 			catch (Exception ex) {
 				amountDiscountedField.setText("Improper Input");
